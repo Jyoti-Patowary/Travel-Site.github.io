d742ed325a3163d5456e5b97f5a92ae7b056d541
Jyoti-Patowary <61742151+Jyoti-Patowary@users.noreply.github.com>
2022-07-25 17:38:34 +0530

    6th

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 
-window.onscroll = function() {
-   myfunction()
-};
-
-var navbar = document.getElementsByClassName("container");
-var sticky = navbar.offsetTop;
-
-function myfunction(){
-   if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
-   } else {
-      navbar.classList.remove("sticky");
-   }
+var navbar = document.getElementById("container")
+// var menu = document.getElementById("menu")
+ console.log(navbar);
+ 
+window.addEventListener("scroll",() => {
+// console.log('scrolled');
+const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+const scrolled = window.scrollY;
+// console.log(scrolled);
+ 
+if(scrolled > 550){
+navbar.classList.add("sticky");
 }
-
+else{
+   navbar.classList.remove("sticky");
+}
+});
